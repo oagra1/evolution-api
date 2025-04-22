f6e51ff725c3721101bd913fc931e4ac5d61a1f3
oagra1 <gustavoagranomade@gmail.com>
2025-04-22 15:27:53 -0300

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'tsup';
+import tsconfigPaths from 'tsconfig-paths';
+
+tsconfigPaths.register(); // 👈 importante
 
 export default defineConfig({
   entry: ['src/index.ts'],
+  splitting: false,
+  clean: true,
+  dts: true,
   format: ['cjs', 'esm'],
   target: 'es2020',
-  sourcemap: true,
-  dts: true, // Continua gerando tipos (agora sem erro)
-  clean: true,
-  splitting: false,
-  minify: false
+  tsconfig: './tsconfig.json',
+  outDir: 'dist',
+  shims: false,
+  treeshake: true,
+  skipNodeModulesBundle: true
 });
