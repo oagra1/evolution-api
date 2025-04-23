3f804cf491a82c834effb774849d9ae9a7d00ba5
oagra1 <gustavoagranomade@gmail.com>
2025-04-22 21:24:59 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { app } from './config/app';
+import express from 'express';
+import publicRoutes from '@api/public.controller';
 
+const app = express();
+
+app.use(express.json());
+app.use('/api', publicRoutes);
+
+// porta configurável via .env ou default 8080
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server listening on port ${PORT}`);
+  console.log(`🚀 Evolution API rodando em http://localhost:${PORT}`);
 });
